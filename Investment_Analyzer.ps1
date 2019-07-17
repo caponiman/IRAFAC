3a6d4c98be0fde51ce6498e1c8b2c466bc760e5f
caponiman <52828630+caponiman@users.noreply.github.com>
2019-07-17 10:25:20 -0400

    Add files via upload

---
+++ Investment_Analyzer.ps1
@@ -0,0 +1,8 @@
+﻿#Investor Ratio and Financial Assessment Calculator
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$Main = New-Object System.Windows.Forms.Form
+$Main.text = "IRAFAC"
+
+$Main.ShowDialog()
